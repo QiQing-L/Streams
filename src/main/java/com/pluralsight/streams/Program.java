89c489b1ec86e1b2013a6652496fb497fa79b88d
Qi Qing <learn.qiqing@gmail.com>
2025-11-04 09:59:50 -0500

    completed person class. added main class, added 10 person to list

---
+++ src/main/java/com/pluralsight/streams/Program.java
@@ -0,0 +1,26 @@
+package com.pluralsight.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        // list of 10 people:
+
+        List<Person> peopleList = new ArrayList<>();
+        peopleList.add(new Person("Carissa", "Carrillo",20));
+        peopleList.add(new Person("Ines", "Harrison",30));
+        peopleList.add(new Person("Callie", "Werner",40));
+        peopleList.add(new Person("Martina", "Saunders",23));
+        peopleList.add(new Person("Brooks", "Ramos",28));
+        peopleList.add(new Person("Malcolm", "Wilcox",10));
+        peopleList.add(new Person("Brigitte", "Herrera",2));
+        peopleList.add(new Person("Terrell", "Rios",70));
+        peopleList.add(new Person("Kayla", "Novak",66));
+        peopleList.add(new Person("Kathleen", "Brown",95));
+
+
+
+
+    }
+}
