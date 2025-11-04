b124e2cda153fda056635a493e7d84e60baaeaad
Qi Qing <learn.qiqing@gmail.com>
2025-11-04 10:10:35 -0500

    Prompt the user for a name to search (first or last). added loop to display name list of person matched search

--- src/main/java/com/pluralsight/streams/Program.java
+++ src/main/java/com/pluralsight/streams/Program.java
@@ -2,6 +2,7 @@ package com.pluralsight.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
@@ -10,14 +11,33 @@ public class Program {
         List<Person> peopleList = new ArrayList<>();
         peopleList.add(new Person("Carissa", "Carrillo",20));
         peopleList.add(new Person("Ines", "Harrison",30));
-        peopleList.add(new Person("Callie", "Werner",40));
+        peopleList.add(new Person("Callie", "Brooks",40));
         peopleList.add(new Person("Martina", "Saunders",23));
         peopleList.add(new Person("Brooks", "Ramos",28));
         peopleList.add(new Person("Malcolm", "Wilcox",10));
-        peopleList.add(new Person("Brigitte", "Herrera",2));
+        peopleList.add(new Person("Brigitte", "Brown",2));
         peopleList.add(new Person("Terrell", "Rios",70));
         peopleList.add(new Person("Kayla", "Novak",66));
         peopleList.add(new Person("Kathleen", "Brown",95));
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name (first or last name):");
+        String nameInput = scanner.nextLine().trim().toLowerCase();
+
+        List<Person> searchList = new ArrayList<>();
+        for (Person person :peopleList){
+            if (person.getFirstName().equalsIgnoreCase(nameInput)
+                    || person.getLastName().equalsIgnoreCase(nameInput)){
+                searchList.add(person);
+            }
+        }
+
+        for (Person personOnList: searchList){
+            System.out.println(personOnList);
+        }
+
+
 
 
 
